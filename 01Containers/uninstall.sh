959814399f83bfa4faeee491c8e93c231e970fe3
zhangguangyong <1243610991@qq.com>
2019-10-15 14:59:00 +0800

    1. 添加安装脚本与卸载脚本

--- 01Containers/uninstall.sh
+++ 01Containers/uninstall.sh
@@ -7,7 +7,3 @@ docker rm $(docker container ls -a | grep customized | awk '{print $1}')
 
 # 删除容器镜像
 docker rmi $(docker images | grep customized | awk '{print $3}')
-
-# 删除网络环境
-docker network rm c001
-docker network rm c101
