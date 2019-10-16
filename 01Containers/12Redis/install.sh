184f29cad837ee2757631aa6bbf03cedb8849ff8
zhangguangyong <1243610991@qq.com>
2019-10-16 11:04:27 +0800

    1. add db containers

--- 01Containers/12Redis/install.sh
+++ 01Containers/12Redis/install.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
+
+v_dir_current=$(cd `dirname $0`; pwd)
+cd ${v_dir_current}/
+
+# 下载配置模板(wget https://raw.githubusercontent.com/antirez/redis/5.0.6/redis.conf -O redis.conf)
+wget http://download.redis.io/redis-stable/redis.conf -O redis.conf
+
+# 修改配置
+sed -i 's/logfile ""/logfile "access.log"/' redis.conf
+sed -i 's/protected-mode yes/protected-mode no/' redis.conf
+sed -i 's/appendonly no/appendonly yes/' redis.conf
+
 docker-compose up -d
