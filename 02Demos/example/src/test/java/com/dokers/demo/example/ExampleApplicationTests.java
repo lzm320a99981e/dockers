380e3ed51af9a8ecec141af3e3fe4b78f985f560
zhangguangyong <1243610991@qq.com>
2019-10-27 23:51:49 +0800

    add container elk 02

---
+++ 02Demos/example/src/test/java/com/dokers/demo/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokers.demo.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
