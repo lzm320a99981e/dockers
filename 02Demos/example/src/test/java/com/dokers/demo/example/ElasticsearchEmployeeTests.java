54b7bdddbf65c23bc14beaeb19b3d29dce1877d1
zhangguangyong <1243610991@qq.com>
2019-12-18 16:05:52 +0800

    1. elastic demo 04

--- 02Demos/example/src/test/java/com/dokers/demo/example/ElasticsearchEmployeeTests.java
+++ 02Demos/example/src/test/java/com/dokers/demo/example/ElasticsearchEmployeeTests.java
@@ -2,6 +2,8 @@ package com.dokers.demo.example;
 
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Maps;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -22,17 +24,21 @@ public class ElasticsearchEmployeeTests {
 
     @Test
     void testIndexApi() throws IOException {
-//        client.index()
-
         final Map<String, Object> source = Maps.newHashMap();
         source.put("user", "kimchy");
         source.put("postDate", new Date());
         source.put("message", "trying out Elasticsearch");
-        final IndexRequest indexRequest = new IndexRequest("employee");
+        final IndexRequest indexRequest = new IndexRequest("employees", "doc");
         indexRequest.source(source);
 
         final IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
         System.out.println(JSON.toJSONString(indexResponse, true));
+    }
 
+    @Test
+    void testGetApi() throws IOException {
+        final GetRequest request = new GetRequest("employees", "doc", "DEREG24B_76fhNV5LOAE");
+        final GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        System.out.println(JSON.toJSONString(response, true));
     }
 }
